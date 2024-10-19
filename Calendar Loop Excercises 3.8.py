1809279816881830a2a27606733598f9d8b4e312
RichieMunro <richie.munro@outlook.com>
2024-10-19 22:26:16 +1000

    Add files via upload
    Had to upload this program.

---
+++ Calendar Loop Excercises 3.8.py
@@ -0,0 +1,3 @@
+for months in ["January", "February", "March", "April", "June", "July", "August", "September", "October", "November", "December"]:
+    calendar = "One of the months in the year is " + months 
+    print(calendar)
